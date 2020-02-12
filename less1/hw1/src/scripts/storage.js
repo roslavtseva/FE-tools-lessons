48c4ed1e7ab780d655d8f2b254b4f3962eb1c7ce
Macbook Pro <nastyarosl@gmail.com>
2020-02-12 17:03:24 +0200

    first commit

---
+++ less1/hw1/src/scripts/storage.js
@@ -0,0 +1,36 @@
+import { renderEvents } from './render-events.js'
+
+export { setItemToStorage, getItemFromStorage, getEventsFromLocal, deleteEvent };
+
+const setItemToStorage = (key, value) => {
+    localStorage.setItem(key, JSON.stringify(value));
+}
+const getItemFromStorage = (key) => JSON.parse(localStorage.getItem(key));
+
+function getEventsFromLocal() {
+    return getItemFromStorage('eventsList') || [];
+  }
+  
+
+function onStorageChange(event) {
+    if (event.key === 'events') {
+        renderEvents();
+    }
+}
+window.addEventListener('storage', onStorageChange);
+
+function deleteEvent(eventId) {
+    const eventsList = getEventsFromLocal();
+    let eventIndex;
+
+    eventsList.find(({ id }, index) => {
+            if (id === eventId) {
+                eventIndex = index;
+                return true;
+            }
+    });
+    eventsList.splice(eventIndex, 1);
+    setItemToStorage('eventsList', eventsList);
+
+};
+    
